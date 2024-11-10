8823f000c1b9a9b051adfa9ec2715303ccfd21bb
amitkie <amit.k@kiesquare.com>
2024-11-10 07:30:15 +0000

    insights data fixes

--- src/pages/insights/InisghtsTabular.jsx
+++ src/pages/insights/InisghtsTabular.jsx
@@ -1,35 +1,58 @@
-// InsightsTabular.js
 import React from "react";
 import Table from "react-bootstrap/Table";
 import "./InsightsTabular.scss";
 
-const InsightsTabular = ({ data, columns, projectName }) => {
+const InsightsTabular = ({ data, columns }) => {
+  const uniqueBrands = Array.from(
+    new Set(
+      data
+        .flatMap((project) =>
+          project.brands ? project.brands.map((item) => item.brand_name) : []
+        )
+    )
+  );
+
   return (
-    <>
-      <h3>{projectName}</h3>
-      <Table responsive striped bordered>
-        <thead>
-          <tr>
-            {columns.map((column, index) => (
-              <th key={index}>{column.header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, rowIndex) => (
-            <tr className="c-pointer" key={rowIndex}>
-              {columns.map((column, colIndex) => (
-                <td key={`${rowIndex}-${colIndex}`}>
-                  {typeof row[column.accessor] === "number"
-                    ? row[column.accessor].toFixed(2)
-                    : row[column.accessor]}
-                </td>
-              ))}
-            </tr>
+    <Table responsive striped bordered className="insights-table">
+      <thead>
+        <tr>
+          <th className="sticky-brand sticky-brand-header">Brands</th>
+          {data.map((project, projIndex) => (
+            <th key={projIndex} colSpan={columns.length} className="project-header">
+              {project.project_name}
+            </th>
           ))}
-        </tbody>
-      </Table>
-    </>
+        </tr>
+        <tr>
+          <th className="sticky-brand sticky-brand-header"></th>
+          {data.map(() =>
+            columns.map((column, colIndex) => (
+              <th key={colIndex}>{column.header}</th>
+            ))
+          )}
+        </tr>
+      </thead>
+      <tbody>
+        {uniqueBrands.map((brand, brandIndex) => (
+          <tr key={brandIndex}>
+            <td className="sticky-brand">{brand}</td>
+            {data.map((project, projIndex) => {
+              const brandData = project.brands
+                ? project.brands.find((item) => item.brand_name === brand)
+                : null;
+
+              return columns.map((column, colIndex) => (
+                <td key={`${projIndex}-${colIndex}`}>
+                  {brandData && brandData.dq_score[column.accessor] !== undefined
+                    ? parseFloat(brandData.dq_score[column.accessor]).toFixed(2)
+                    : "N/A"}
+                </td>
+              ));
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 };
 
